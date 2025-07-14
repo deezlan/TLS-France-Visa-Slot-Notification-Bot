0f06739c6937427ea8bec99cc2f2b3e033338b59
Danial Eizlan Daud <64958824+deezlan@users.noreply.github.com>
2025-07-14 17:23:54 +0800

    Added arguments to browser launch in quickstart.py

--- quickstart.py
+++ quickstart.py
@@ -2,7 +2,10 @@ from playwright.sync_api import sync_playwright
 
 pw = sync_playwright().start()
 
-browser = pw.firefox.launch()
+browser = pw.firefox.launch(
+    headless=False,  # Set to False if you want to see the browser
+    # slow_mo=2000
+)
 
 page = browser.new_page()
 page.goto("https://www.google.com")
