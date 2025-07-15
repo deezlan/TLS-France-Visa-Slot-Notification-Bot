8b713a78d32a5e9556dd0ed20046367921e4d644
Danial Eizlan Daud <64958824+deezlan@users.noreply.github.com>
2025-07-15 16:03:29 +0800

    Added navigation to preferred UK centre

--- quickstart.py
+++ quickstart.py
@@ -1,17 +1,28 @@
 from playwright.sync_api import sync_playwright 
 
-pw = sync_playwright().start()
+centre_index = 1 # Edinburgh = 0, London = 1, Manchester = 2
+email = "usvi980@outlook.com" # Account email
+password = "123456Asdfgh!" # Account password
 
-browser = pw.firefox.launch(
-    headless=False,  # Set to False if you want to see the browser
-    # slow_mo=2000
-)
+with sync_playwright() as pw:
+    browser = pw.firefox.launch(
+        headless=True,  # Set to False if you want to see the browser
+        slow_mo=2000
+    )
 
-page = browser.new_page()
-page.goto("https://www.google.com")
+    page = browser.new_page()
+    
+    # Initial page
+    page.goto("https://visas-fr.tlscontact.com/country/gb/?lang=en-us")
+    page.wait_for_load_state("load")
+    print("Centre Selection Page Loaded")
+    page.screenshot(path="screenshots/Centre Selection.png")
 
-print(page.content())
-print(page.title())
-page.screenshot(path="screenshot.png")
+    # Navigate to preferred centre
+    with page.expect_navigation():
+        page.get_by_role("button").get_by_text("Continue").nth(centre_index).click()
+    page.wait_for_load_state("load")
+    print("Centre Home Page Loaded")
+    page.screenshot(path="screenshots/Centre Home.png")
 
-browser.close()
+    browser.close()
