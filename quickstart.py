6613b39c75b0309d816f4cd6b21f701b22c2fbf4
Danial Eizlan Daud <64958824+deezlan@users.noreply.github.com>
2025-07-15 16:25:53 +0800

    Switched to wait_for_selector for accurate screenshots

--- quickstart.py
+++ quickstart.py
@@ -7,21 +7,21 @@ password = "123456Asdfgh!" # Account password
 with sync_playwright() as pw:
     browser = pw.firefox.launch(
         headless=True,  # Set to False if you want to see the browser
-        slow_mo=2000
+        # slow_mo=2000
     )
 
     page = browser.new_page()
     
     # Initial page
     page.goto("https://visas-fr.tlscontact.com/country/gb/?lang=en-us")
-    page.wait_for_load_state("load")
+    page.wait_for_selector("button:has-text('Continue')")
     print("Centre Selection Page Loaded\nLink: " + page.url)
     page.screenshot(path="screenshots/Centre Selection.png")
 
     # Navigate to preferred centre
     with page.expect_navigation():
         page.get_by_role("button").get_by_text("Continue").nth(centre_index).click()
-    page.wait_for_load_state("load")
+    page.wait_for_selector("h1")
     print("Centre Home Page Loaded\nLink: " + page.url)
     page.screenshot(path="screenshots/Centre Home.png")
 
